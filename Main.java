2f3cace7bad07a77a3c9e23bfb7a922c598a3d0d
Ashley  Grafner <>
2022-10-17 20:12:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+//Ashley Grafner
+//CSC 10606080, Computer Science 1 (Java)
+//10/13/2022
+//implements the UML Class attached to this project called Coffee Dispenser.png.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) { //main method
+  
+   static coffeeDispenser{
+     
+   }
+        
+
+
+
+  
+  
+}
+
+
+
+
+      
+    }
+}
+
+  
---
+++ README.md
@@ -0,0 +1,1 @@
+Create the Java code to implement the UML Class attached to this project called Coffee Dispenser.png.
